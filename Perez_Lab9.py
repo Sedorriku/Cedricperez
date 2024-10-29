a0044e755c918be9ae72933fb2fc1ff173005236
Sedorriku <cirdeczerep@gmail.com>
2024-10-29 06:44:10 -0700

    Add files via upload

---
+++ Perez_Lab9.py
@@ -0,0 +1,8 @@
+rows=int(input("Enter The Number of Rows: " ))
+base=int(0)
+
+for rows in range(rows + 1):
+    for rows in range(rows + 1):
+        base += 1
+        print(base, end=" ")
+    print()
